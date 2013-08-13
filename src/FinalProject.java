40db933dfb6c8c0cbbc050e4d33939606d1a7767
gman45 <garhowe45@gmail.com>
2013-08-13 11:32:01 -0400

    hi

---
+++ src/FinalProject.java
@@ -0,0 +1,12 @@
+
+public class FinalProject {
+//Stratego Project... make different characters that have different values
+	//Make bombs that cannot move but kill everyone but 3's
+	//Nobody but bombs and spies can kill the 10
+	//If two characters that have the same value attack each other... they both die.
+	
+	public static void main(String[] args) {
+System.out.println("Hello");
+	}
+
+}
